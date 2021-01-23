5d3e95ab62a27f5e2b0545a51678190fda221f8e
kebekchieva <kebekchieva.ai@gmail.com>
2021-01-23 14:36:16 +0600

    added second test page and url

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 
 def test(request):
     return HttpResponse("hi")
+
+def second(request):
+    return HttpResponse("test2 page")
--- todo/urls.py
+++ todo/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import test
+from main.views import test, second
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("test", test, name="test")
+    path("test", test, name="test"),
+    path("test2", second)
 ]
