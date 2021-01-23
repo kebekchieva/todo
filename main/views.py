040824a1c5b67fb1bf5e17c174a208109666619c
kebekchieva <kebekchieva.ai@gmail.com>
2021-01-23 16:27:32 +0600

    copied index.html

--- main/views.py
+++ main/views.py
@@ -2,10 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 
 def test(request):
-    return HttpResponse("hi")
-
-def second(request):
-    return HttpResponse("test2 page")
-
-def hw3(request):
-    return HttpResponse("This is page test3")
+    return render(request, "index.html")
--- todo/urls.py
+++ todo/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("test", test, name="test"),
-    path("test2", second),
-    path("third", hw3)
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)\
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
